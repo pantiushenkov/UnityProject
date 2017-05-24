ad3ffa8756b7a7402ad1208d23d454fde24ffb49
VLad <pantiushenkov@gmail.com>
2017-05-24 13:43:00 +0300

    AnimationController and LevelController added

---
+++ Assets/LevelController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+	public static LevelController current;
+	// Use this for initialization
+	void Awake () {
+		current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
